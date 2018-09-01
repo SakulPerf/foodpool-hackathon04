baaa6cd875f7cca5fe6b83b3b2b797609072a383
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2018-09-01 12:06:22 +0700

    Fix all

--- foodpool/src/pages/shop-detail/shop-detail.ts
+++ foodpool/src/pages/shop-detail/shop-detail.ts
@@ -12,7 +12,7 @@ import { ShopInfo } from '../../models/models';
 })
 export class ShopDetailPage {
 
-  shopInfo: ShopInfo;
+  shopInfo: ShopInfo = new ShopInfo();
 
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
@@ -30,7 +30,7 @@ export class ShopDetailPage {
     this.items = [];
     for(let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
+        title: 'MyItem ' + i,
         note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
